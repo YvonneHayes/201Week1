5b10cd74046926cae491bf9b18fdd67fb28204b0
Yvonne Hayes <queyvonne@gmail.com>
2016-03-15 11:29:59 -0700

    comitted new files

---
+++ app.js
@@ -0,0 +1,33 @@
+//Tuesday Lab Exercise
+
+//Question1
+var userName2 = prompt("What is your name?");
+console.log("Welcome " + userName2 + " !");
+
+//Question2
+var userAge = prompt("How old are you?");
+if (userAge < 30) {
+  console.log("Oh, you little Whippersnapper!");
+} else if (userAge <= 45) {
+  console.log("What a great age to be!");
+} else {
+  console.log("With age comes wisdom, right? Right???");
+}
+
+//Question3
+var userOrigin = prompt("Where are you from?");
+console.log(userOrigin + " is a great place to be from!");
+
+//Question4
+var userAnimal = prompt("Do you prefer dogs or cats?");
+if (userAnimal === 'cats'){
+  console.log("Meow. So you like being treated like a slave. Nice.");
+} else if (userAnimal === 'dogs') {
+  console.log("Right on! Dogs rule!");
+} else {
+  console.log("You had two choices, mate. Two!");
+}
+
+//Question4
+
+var userDrink = prompt("Do you like ?")
