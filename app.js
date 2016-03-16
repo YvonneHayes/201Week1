8803ce30e0ab46c13647e01c445c93e0c3ce9b38
Yvonne Hayes <queyvonne@gmail.com>
2016-03-16 10:23:21 -0700

    Updated Lab exercise

--- app.js
+++ app.js
@@ -1,53 +1,55 @@
-//Tuesday Lab Exercise
+//Wednesday Lab Exercise
 
+//User Name
+var userName = prompt('Please tell me your name!');
+console.log('The user\'s name is ' + userName);
 
-//Question1
-var userName2 = prompt("What is your name?");
-console.log("Welcome " + userName2 + " !");
+//Welcome Message
+alert('Welcome, ' + userName + ' Let\'s play a game. I\'m going to ask you some questions about me. Please click "ok" and then we\'ll get started!');
 
-//Question2
-var userAge = prompt("How old are you?");
-if (userAge < 30) {
-  console.log("Oh, you little Whippersnapper!" + userAge + " is nothing!");
-} else if (userAge <= 45) {
-  console.log(userAge + " is a great age to be!");
+//First Question - Yes or No
+var userResponse1 = prompt('Am I a passionate cook who loves creating tasty treats for friends and family? Please answer Y or N.');
+console.log('userResponse1: ' + userResponse1);
+
+if (userResponse1.toLowerCase() === 'n' || userResponse1.toUpperCase() === 'NO') {
+  alert('You are correct. Cooking is not one of my many fantastic talents. Don\'t worry - we\'ll order in!');
+} else if (userResponse1.toUpperCase() === 'Y' || userResponse1.toLowerCase() === 'no') {
+  alert('Sorry, domestic Goddess I am not. No worries, we\'ll order a delicious Pizza if you come to my house.');
 } else {
-  console.log("Wow! " + userAge + ", huh? With age comes wisdom, right? Right???");
+  alert('Please enter either "y" or "n"!');
 }
 
-//Question3
-var userOrigin = prompt("Where are you from?");
-console.log(userOrigin + " is a great place to be from!");
+//Second Question - Yes or No
+var userResponse2 = prompt('Am I a punctual person? Please answer Y or N.');
+console.log('userResponse2: ' + userResponse2);
 
-//Question4
-var userAnimal = prompt("Do you prefer dogs or cats?");
-if (userAnimal === 'cats'){
-  console.log("Meow. So you like being treated like a slave. Nice. Enjoy your " + userAnimal + " !");
-} else if (userAnimal === 'dogs') {
-  console.log("Right on! " + userAnimal + " rule!");
+if (userResponse2.toLowerCase() === 'n' || userResponse2.toLowerCase() === 'no') {
+  alert('Sorry, you are very wrong on this one. I am pathologically punctual - and usually early.');
+} else if(userResponse2.toUpperCase() === 'Y' || userResponse2.toUpperCase() === 'YES') {
+  alert('You are right! It must be my germanic roots. I\'m usually at least 10 minutes early.');
 } else {
-  console.log("You had two choices, mate. Two!");
+  alert('Please enter either "y" or "n"!');
 }
 
-//Question5
-var userDrink = prompt("Do you prefer wine or beer?");
-if(userDrink === 'beer'){
-  console.log("Great! If you like " + userDrink + " Portland is the right place for you!");
-} else {
-  console.log(userDrink + " sounds delicious!");
+//Third Question - Switch Statement
+var userResponse3 = prompt('What was my first pet? A hamster, a bunny, a dog or a cat?');
+console.log('userResponse3: ' + userResponse3);
+
+var pets = userResponse3.toLowerCase();
+
+switch (pets) {
+  case 'hamster':
+    alert('Sorry, but no. A hamster was my second pet.');
+    break;
+  case 'bunny':
+    alert('That\'s right! My first pet was a black bunny rabbit named Stupsi.');
+    break;
+  case 'dog':
+    alert('Oh, I wish! But no I had to wait many years until finally being able to get a dog.');
+    break;
+  case 'cat':
+    alert('Sorry, but you got this one wrong. The cats are a very new addition to my life.');
+    break;
+  default:
+    alert('Please pick out of the four choices: hamster, bunny, dog or cat!');
 }
-
-//Question6
-var userTV = prompt("What's your favorite TV show?");
-console.log("So you like " + userTV + ", huh? Can't argue taste, can you?");
-
-//Question7
-
-var userPortland = prompt("How many years have you lived in Portland?");
-if (userPortland < 5) {
-  console.log("Only " + userPortland + " years? You are still a newbie!");
-} else {
-  console.log("After " + userPortland + " years you are a true local. You should show me around!");
-}
-
-alert("Thank you for answering my questions! I'll be over soon at your place with some " + userDrink + " to watch some " + userTV + " !");
